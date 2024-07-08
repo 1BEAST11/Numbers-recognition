9b13028744e20f9901aab158dd8637d2745f1c94
Satyukova Kseniya <73394587+1BEAST11@users.noreply.github.com>
2024-07-08 18:36:03 +0500

    Add files via upload

---
+++ 4(1).cpp
@@ -0,0 +1,131 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+
+using namespace std;
+using namespace cv;
+
+double activate(double S)
+{
+    return 1 / (1 + exp(-S)); // функция активации - сигмоид
+}
+
+vector<vector<double>> readCSV(const string& filename)
+{
+    vector<vector<double>> data; // Двумерный вектор для хранения данных
+
+    ifstream file(filename);
+    if (!file.is_open())
+    {
+        cerr << "Ошибка при открытии файла!" << endl;
+        return data;
+    }
+
+    string line;
+
+    // Чтение файла построчно
+    while (getline(file, line))
+    {
+        vector<double> row;
+        istringstream iss(line);
+        string cell;
+
+        // Разделение строки на ячейки по разделителю ';'
+        while (getline(iss, cell, ';'))
+        {
+            row.push_back(stod(cell)); // Преобразование строки в число типа double и добавление в вектор 
+        }
+
+        data.push_back(row); // Добавление строки в двумерный вектор
+    }
+
+    return data;
+}
+
+int main()
+{
+    string new_1 = "D:/Учеба/Машинное обучение и нейронные сети/4/weights_1.csv";
+    string new_2 = "D:/Учеба/Машинное обучение и нейронные сети/4/weights_2.csv";
+
+    vector <vector<double>> weights_1(785, vector<double>(81)); // веса для in -> hidden
+    vector<vector<double>> weights_2(81, vector<double>(10)); // веса для hidden -> out
+
+    weights_1 = readCSV (new_1); // веса для in -> hidden
+    weights_2 = readCSV(new_2); // веса для hidden -> out
+    int choice = 0;
+
+
+    while (true)
+    {
+        string filePath;
+        cout << "Input path to your picture: ";
+        cin >> filePath;
+
+        vector<double> in(785, 0.0); // вход
+        vector<double> hidden(81, 0.0); // скрытый слой
+        vector <double> out(10, 0.0); // выход
+
+        // считываем изображение с помощью OpenCV
+        Mat image = imread(filePath, IMREAD_GRAYSCALE);
+
+        // bitwise_not(image, image);
+        threshold(image, image, 0, 1, THRESH_BINARY);
+        int count = 0;
+        for (int i = 0; i < image.rows; ++i)
+        {
+            for (int j = 0; j < image.cols; ++j)
+            {
+                double pixel = static_cast<double>(image.at<uchar>(i, j));
+                //cout << pixel << " ";
+                in[count] = pixel; // выходы нейронов слоя слоя "in"
+                count++;
+            }
+            //cout << endl;
+        }
+        in[784] = 1.0; // нейрон смещения слоя "in"
+
+        // расчет значений для скрытого слоя
+        vector <double> S_hidden(81, 0.0);
+        for (int i = 0; i < hidden.size(); i++)
+        {
+            for (int j = 0; j < in.size(); j++)
+            {
+                S_hidden[i] += in[j] * weights_1[j][i]; // расчет входа нейрона слоя "hidden"
+            }
+
+            hidden[i] = activate(S_hidden[i]); // расчет выхода нейрона слоя "hidden"
+        }
+        hidden[80] = 1.0;
+
+        // расчет значений для выходного слоя
+        vector <double> S_out(10, 0.0);
+        for (int i = 0; i < out.size(); i++)
+        {
+            for (int j = 0; j < hidden.size(); j++)
+            {
+                S_out[i] += hidden[j] * weights_2[j][i]; // расчет входа нейрона слоя "out"
+            }
+            out[i] = activate(S_out[i]); // расчет выхода нейрона слоя "out"
+           cout << i << ") " << out[i] << endl;
+        }
+        cout << endl;
+        // Находим итератор на максимальный элемент вектора
+        auto max_it = max_element(out.begin(), out.end());
+
+        // Получаем индекс максимального элемента
+        int max_index = distance(out.begin(), max_it);
+
+        cout << "Number on your picture: " << max_index << endl << endl;
+
+        cout << "Do you want to continue? (1 - yes; 0 - no) ";
+        cin >> choice;
+
+        if (choice == 0)
+        {
+            break;
+        }
+    }
+
+    return 0;
+}
+
---
+++ 4.cpp
@@ -0,0 +1,215 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <filesystem>
+#include <random>
+#include <string>
+#include <fstream>
+
+namespace fs = std::filesystem;
+using namespace std;
+using namespace cv;
+
+double sigmoid(double S)
+{
+    return 1 / (1 + exp(-S)); // функция активации - сигмоид
+}
+
+vector<vector<double>> readCSV(const string& filename)
+{
+    vector<vector<double>> data; // Двумерный вектор для хранения данных
+
+    ifstream file(filename);
+    if (!file.is_open())
+    {
+        cerr << "Ошибка при открытии файла!" << endl;
+        return data;
+    }
+
+    string line;
+
+    // Чтение файла построчно
+    while (getline(file, line))
+    {
+        vector<double> row;
+        istringstream iss(line);
+        string cell;
+
+        // Разделение строки на ячейки по разделителю ';'
+        while (getline(iss, cell, ';'))
+        {
+            row.push_back(stod(cell)); // Преобразование строки в число типа double и добавление в вектор 
+        }
+
+        data.push_back(row); // Добавление строки в двумерный вектор
+    }
+
+    file.close();
+    return data;
+}
+
+int main()
+{
+    vector <vector<double>> weights_1(785, vector<double>(81)); // веса для in -> hidden
+    vector <vector<double>> weights_2(81, vector<double>(10)); // веса для hidden -> out
+
+    weights_1 = readCSV("new_1.csv"); // веса для in -> hidden
+    weights_2 = readCSV("new_2.csv"); // веса для hidden -> out
+
+    random_device rd;
+    mt19937 generator(rd()); // генератор случайных чисел
+    uniform_real_distribution<double> distribution(-0.5, 0.5);
+
+    ofstream new_1("weights_1.csv");
+    ofstream new_2("weights_2.csv");
+
+    //// генеририруем веса для in -> hidden
+    //for (int i = 0; i < 785; i++)
+    //{
+    //    for (int j = 0; j < 81; j++)
+    //    {
+    //        weights_1[i][j] = distribution(generator); // веса связей между нейронами
+    //        old_1 << setprecision(32) << weights_1[i][j] << ";";
+    //    }
+    //    old_1 << endl;
+    //}
+
+    //// генерируем веса для hidden->out
+    //for (int i = 0; i < 81; i++)
+    //{
+    //    for (int j = 0; j < 10; j++)
+    //    {
+    //        weights_2[i][j] = distribution(generator); // веса связей между нейронами
+    //        old_2 << weights_2[i][j] << ";";
+    //    }
+    //    old_2 << endl;
+    //}
+
+    for (int epoch = 0; epoch < 5; epoch++)
+    {
+        cout << epoch << ") " << endl;
+        string folderPath = "training";
+
+        // цикл для поочередного чтения файлов из папки
+        for (const auto& entry : fs::directory_iterator(folderPath))
+        {
+            vector<double> in(785, 0.0); // вход
+            vector<double> hidden(81, 0.0); // скрытый слой
+
+            vector<double> out(10, 0.0); // выход
+            vector<double> target(10, 0.0); // целевое значение
+
+
+            // получаем путь к текущему файлу
+            string filePath = entry.path().string();
+            cout << "Reading file: " << filePath << endl;
+
+            char ch = filePath[filePath.size() - 5];
+            int number = (ch == '0') ? 0 : ch - '0';
+            target[number] = 1.0;
+
+            for (int i = 0; i < target.size(); i++)
+            {
+                cout << target[i] << " ";
+            }
+            cout << endl;
+
+            //ПРЯМОЙ ПРОГОН
+            // считываем изображение с помощью OpenCV
+            Mat image = imread(filePath, IMREAD_GRAYSCALE);
+
+            threshold(image, image, 0, 1, THRESH_BINARY);
+            int count = 0;
+
+            for (int i = 0; i < image.rows; ++i)
+            {
+                for (int j = 0; j < image.cols; ++j)
+                {
+                    double pixel = static_cast<double>(image.at<uchar>(i, j));
+                    in[count] = pixel; // выходы нейронов слоя слоя "in"
+                    count++;
+                }
+            }
+            in[784] = 1.0; // нейрон смещения слоя "in"
+
+            // расчет значений для скрытого слоя
+            vector <double> S_hidden(81, 0.0);
+            for (int i = 0; i < hidden.size(); i++)
+            {
+                for (int j = 0; j < in.size(); j++)
+                {
+                    S_hidden[i] += in[j] * weights_1[j][i]; // расчет входа нейрона слоя "hidden"
+                }
+
+                hidden[i] = sigmoid(S_hidden[i]); // расчет выхода нейрона слоя "hidden"
+            }
+            hidden[80] = 1.0;
+
+            // расчет значений для выходного слоя
+            vector <double> S_out(10, 0.0);
+            for (int i = 0; i < out.size(); i++)
+            {
+                for (int j = 0; j < hidden.size(); j++)
+                {
+                    S_out[i] += hidden[j] * weights_2[j][i]; // расчет входа нейрона слоя "out"
+                }
+                out[i] = sigmoid(S_out[i]);
+                cout << out[i] << endl;
+            }
+            cout << endl;
+
+            // ОБРАТНЫЙ ПРОГОН (BACK PROPAGATION)
+            double lambda = 0.1;
+            vector <double> dE_dS_1(81, 0.0);
+
+            // изменение весов для hidden -> out
+            for (int i = 0; i < out.size(); i++)
+            {
+                double error_2 = 2 * (target[i] - out[i]) * out[i] * (1 - out[i]);
+
+                for (int j = 0; j < hidden.size(); j++)
+                {
+                    weights_2[j][i] += lambda * error_2 * hidden[j];
+                    dE_dS_1[j] += weights_2[j][i] * error_2;
+                }
+            }
+
+            // изменение весов для in -> hidden
+            for (int i = 0; i < hidden.size(); i++)
+            {
+                double error_1 = hidden[i] * (1 - hidden[i]) * dE_dS_1[i];
+                for (int j = 0; j < in.size(); j++)
+                {
+                    weights_1[j][i] += lambda * error_1 * in[j];
+
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < 785; i++)
+    {
+        for (int j = 0; j < 81; j++)
+        {
+            new_1 << setprecision(32) << weights_1[i][j] << ";";
+        }
+        new_1 << "\n";
+    }
+
+    for (int i = 0; i < 81; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            new_2 << setprecision(32) << weights_2[i][j] << ";";
+        }
+        new_2 << "\n";
+    }
+
+    
+    cout << "Training complete!" << endl;
+
+    new_1.close();
+    new_2.close();
+
+    return 0;
+}
+
